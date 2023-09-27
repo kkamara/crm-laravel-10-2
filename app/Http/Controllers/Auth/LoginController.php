1b83e6e0b818d4628a947909ac5d9a2a12b3c4d3
kkamara <kamaracomputers@gmail.com>
2023-09-27 15:09:12 +0100

    Refactor Auth Controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,13 +21,6 @@ class LoginController extends Controller
     |
     */
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/dashboard';
-
     protected User|null $user;
 
     /** @var array to store seeder logins for site users */
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -21,13 +21,6 @@ class RegisterController extends Controller
     */
 
     /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
      * Create a new controller instance.
      *
      * @return void
